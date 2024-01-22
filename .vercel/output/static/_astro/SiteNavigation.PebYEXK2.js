a5cf13e5778a6c90f27e849bac3ce14bcd36c25b
Xeretis <70800211+Xeretis@users.noreply.github.com>
2024-01-22 10:37:16 +0100

    Changed the site config

--- .vercel/output/static/_astro/SiteNavigation.PebYEXK2.js
+++
@@ -1,1 +0,0 @@
-import{SiteNavigation as m}from"./SiteNavigation.-zkQUu-O.js";import"./button.PR8U9yCG.js";import"./index.YJRTXu8l.js";export{m as SiteNavigation};
--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://xeretis.github.io",
+    site: "https://xeretis.me",
     integrations: [
         react(),
         tailwind({
