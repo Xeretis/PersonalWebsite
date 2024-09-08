79f3ef240d4d4a2779fd63c414c758da3d6cedb3
Xeretis <70800211+Xeretis@users.noreply.github.com>
2024-09-08 12:44:29 +0200

    Updated the tools I use

--- src/components/home/tools-i-use.tsx
+++ src/components/home/tools-i-use.tsx
@@ -8,10 +8,14 @@ export const ToolsIUse = () => {
             </CardHeader>
             <CardContent>
                 <p className="leading-7 text-muted-foreground">For developing applications:</p>
-                <p className="mb-6 font-mono font-bold">VS Code, IntelliJ Idea Professional, Rider</p>
-                <p className="leading-7 text-muted-foreground">For deploying my finished products:</p>
-                <p className="mb-6 font-mono font-bold">Docker, Nginx</p>
-                <p className="leading-7 text-muted-foreground">For testing and integrating:</p>
+                <p className="mb-6 font-mono font-bold">
+                    VS Code, IntelliJ Idea Professional, Rider
+                </p>
+                <p className="leading-7 text-muted-foreground">
+                    For deploying my finished products:
+                </p>
+                <p className="mb-6 font-mono font-bold">Docker, Nginx, Dokku</p>
+                <p className="leading-7 text-muted-foreground">For testing and CI/CD:</p>
                 <p className="font-mono font-bold">OpenAPI, Postman, Github Actions</p>
             </CardContent>
         </Card>
